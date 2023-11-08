e84d1fdd92f85807c9567fb2afeb07284ae6666e
PotCAR1452 <106353456+PotCAR1452@users.noreply.github.com>
2023-11-08 11:40:27 -0600

    Add files via upload

--- src/db.js
+++ src/db.js
@@ -1,10 +1,20 @@
 import { createPool } from "mysql2/promise";
+import { DB_DATABASE } from "./config.js";
+import { DB_HOST } from "./config.js";
+import { DB_PASSWORD } from "./config.js";
+import { DB_PORT } from "./config.js";
+import { DB_USER } from "./config.js";
 
-export const conn = createPool({
-    user: "root",
-    password: "root",
-    host: "localhost",
-    port: 3306,
-    database: "DECOMUEBLES",
-})
+export let conn;
 
+try {
+    conn = createPool({
+        user: DB_USER,
+        password: DB_PASSWORD,
+        host: DB_HOST,
+        port: DB_PORT,
+        database: DB_DATABASE,
+    });
+} catch (error) {
+    console.error(error);
+}
