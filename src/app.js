fe4ac85a5f0192856dd79c414d5d80579014be33
PotCAR1452 <106353456+PotCAR1452@users.noreply.github.com>
2023-11-27 17:21:15 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,44 @@
 import express from 'express';
-import {conn} from './db.js'
+import {conn} from './db.js';
+import {PORT} from './config.js';
 
 const app = express();
 
 app.get('/', async (req, res) => {
-    
-    res.send('Hello World');
+    res.send('api desplegada');
 })
 
 app.get('/ping', async (req, res) => {
-
-    const result = await conn.query('select "hello" as Result')
-       res.json(result)
-       console.log('ping')
+    
+    const result = await conn.query('select * from tbUsuarios ')
+       res.json(result[0])
+       console.log(result[0])
        
 })
 
- app.listen(3000)
- console.log('server on port', 3000)
+app.post('/insert', async (req, res) => {
+    const { nombre, apellido, email } = req.body;
+    try {
+        const result = await conn.query('INSERT INTO tbUsuarios (nombre, apellido, email) VALUES (?, ?, ?)', [nombre, apellido, email]);
+        res.send('User added successfully');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error adding user to database');
+    }
+});
+
+
+app.get('/check-connection', async (req, res) => {
+    
+    try {
+        const result = await conn.query('SELECT 1');
+        res.send('Connection successful');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error connecting to database');
+    }
+});
+
+
+ app.listen(PORT)
+ console.log('server on port', PORT)
