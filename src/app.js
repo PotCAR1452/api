6a0f648e32fc9da053f89b98b995667887c3f5e2
PotCAR1452 <106353456+PotCAR1452@users.noreply.github.com>
2023-11-27 17:27:18 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,45 @@
 import express from 'express';
 import {conn} from './db.js'
 
+
 const app = express();
 
-app.get('/', async (req, res) => {
-    
+app.get('/', async (_, res) => {
     res.send('Hello World');
-})
+});
 
-app.get('/ping', async (req, res) => {
+app.get('/ping', async (_, res) => {
+    try {
+        const result = await conn.query('select "hello" as Result');
+        res.json(result);
+        console.log('ping');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al obtener datos');
+    }
+});
 
-    const result = await conn.query('select "hello" as Result')
-       res.json(result)
-       console.log('ping')
-       
-})
+app.get('/g/vwMueblessemana', async (_, res) => {
+    try {
+        const result = await conn.query('select * from vwMueblesConFoto');
+        res.json(result[0]);
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al obtener datos');
+    }
+});
 
- app.listen(3000)
- console.log('server on port', 3000)
+app.get('/g/MostrarMuebles', async (_, res) => {
+    try {
+        const result = await conn.query('select * from MostrarMuebles');
+        res.json(result[0]);
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al obtener datos');
+    }
+});
+
+app.listen(3000);
+console.log('server on port', 3000);
