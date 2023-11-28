fe81ff47cf8e377afa9c7b47be70361679927968
PotCAR1452 <106353456+PotCAR1452@users.noreply.github.com>
2023-11-27 20:30:57 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,41 @@
 import express from 'express';
-import {conn} from './db.js'
+import { conn } from './db.js';
+import getRouter from './getRouters.js'; // Importa el enrutador getRouter.js
+import postRouter from './postRoutes.js'; // Importa el enrutador postRouter.js
+import { PORT } from './config.js';
 
 const app = express();
 
+// Middleware para analizar JSON en solicitudes POST
+app.use(express.json());
+
+// Asocia los enrutadores a las rutas
+app.use('/', getRouter);
+app.use('/crud', postRouter);
+
+// Ruta raíz
 app.get('/', async (req, res) => {
-    
     res.send('Hello World');
-})
+});
 
+// Ruta de ping para comprobar la conexión
 app.get('/ping', async (req, res) => {
+    const result = await conn.query('select * from tbUsuarios ')
+    res.json(result[0]);
+    console.log(result);
+});
 
-    const result = await conn.query('select "hello" as Result')
-       res.json(result)
-       console.log('ping')
-       
-})
+// Ruta para comprobar la conexión a la base de datos
+app.get('/check-connection', async (req, res) => {
+    try {
+        const result = await conn.query('SELECT 1');
+        res.send('coneccion exitosa');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error coneccion a la base de datos');
+    }
+});
 
- app.listen(3000)
- console.log('server on port', 3000)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
