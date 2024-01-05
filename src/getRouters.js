05ecf6248c12fb91dd32b48b6abadf4ba0d35778
PotCAR1452 <106353456+PotCAR1452@users.noreply.github.com>
2024-01-05 15:07:53 -0600

    Update getRouters.js parte de la visualizacion del meuble

--- src/getRouters.js
+++ src/getRouters.js
@@ -86,4 +86,36 @@ router.get('/g/mostrarcolores', async (req, res) => {
 })
 
 
+//consultas para parte de modal de visualizacion de la informacion de los muebles
+
+router.get('/g/mostrarmuebleid', async (req, res) => {
+    try {
+        const [idmueble] = req.body;
+        res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // O "*" para permitir cualquier origen
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        const result = await conn.query('call ObtenerInformacionMuebleid(?)', [idmueble]);
+        res.json(result[0]);
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al obtener datos');
+    }
+});
+
+router.get('/g/mostrarfotomueble', async (req, res) => {
+    try {
+        const [idmueble] = req.body;
+        res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // O "*" para permitir cualquier origen
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        const result = await conn.query('call ObtenerFotosMuebleid(?)', [idmueble]);
+        res.json(result[0]);
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al obtener datos');
+    }
+});
+
+
+
 export default router;
